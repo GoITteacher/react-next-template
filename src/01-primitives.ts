ce7511a3f0d90019a42baf9d5d49a0d4ff14492a
Alexander Repeta <alexander.repeta@gmail.com>
2025-05-06 21:04:39 +0300

    Add examples

---
+++ src/01-primitives.ts
@@ -0,0 +1,10 @@
+/**
+ * - Прості типи: any, number, string, boolean, null, undefined
+ * - Виведення типів
+ */
+
+let age: number = 5;
+
+let username: string = "Jacob";
+
+let isOnline: boolean = false;
