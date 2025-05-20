e8466582a575b95ef525ed842b33face8bc3311c
Alexander Repeta <alexander.repeta@gmail.com>
2025-05-20 20:45:11 +0300

    Add examples

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import SearchForm from "./SearchForm/SearchForm";
+import ArticleList from "./ArticleList/ArticleList";
+import { getArticles } from "../services/articleService";
+import { Article } from "../types/article";
+
 export default function App() {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleSearch = async (newTopic: string) => {
+    try {
+      setIsLoading(true);
+      setIsError(false);
+      const newArticles = await getArticles(newTopic);
+      setArticles(newArticles);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
-      <h1>Forms and HTTP requests</h1>
+      <SearchForm onSearch={handleSearch} />
+      {isLoading && <strong>Loading articles...</strong>}
+      {isError && <strong>Something went wrong please try again...</strong>}
+      {articles.length > 0 && <ArticleList items={articles} />}
     </>
   );
 }
--- src/components/OrderForm/OrderForm.tsx
+++ src/components/OrderForm/OrderForm.tsx
@@ -1,29 +1,67 @@
+import { useId } from "react";
 import css from "./OrderForm.module.css";
 
 export default function OrderForm() {
+  const id = useId();
+
+  const handleSubmit = (formData: FormData) => {
+    const values = Object.fromEntries(formData);
+    console.log(values);
+  };
+
   return (
-    <form className={css.form}>
+    <form className={css.form} action={handleSubmit}>
       <fieldset className={css.fieldset}>
         <legend className={css.legend}>Client info:</legend>
-        <label className={css.label}>Name</label>
-        <input className={css.input} type="text" name="username" />
+        <label className={css.label} htmlFor={`${id}-username`}>
+          Name
+        </label>
+        <input
+          className={css.input}
+          type="text"
+          name="username"
+          id={`${id}-username`}
+        />
 
-        <label className={css.label}>Email</label>
-        <input className={css.input} type="email" name="email" />
+        <label className={css.label} htmlFor={`${id}-email`}>
+          Email
+        </label>
+        <input
+          className={css.input}
+          type="email"
+          name="email"
+          id={`${id}-email`}
+        />
       </fieldset>
 
       <fieldset className={css.fieldset}>
         <legend className={css.legend}>Delivery method:</legend>
         <label className={css.option}>
-          <input type="radio" name="delivery" value="pickup" defaultChecked />
+          <input
+            type="radio"
+            name="delivery"
+            value="pickup"
+            id={`${id}-pickup`}
+            defaultChecked
+          />
           Pickup
         </label>
         <label className={css.option}>
-          <input type="radio" name="delivery" value="courier" />
+          <input
+            type="radio"
+            name="delivery"
+            value="courier"
+            id={`${id}-courier`}
+          />
           Courier
         </label>
         <label className={css.option}>
-          <input type="radio" name="delivery" value="drone" />
+          <input
+            type="radio"
+            name="delivery"
+            value="drone"
+            id={`${id}-drone`}
+          />
           Drone delivery
         </label>
       </fieldset>
