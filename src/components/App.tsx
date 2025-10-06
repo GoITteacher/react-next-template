f9267d80f73d18b349f9f9924de82a1612beda5d
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-10-06 07:24:42 +0300

    before

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,9 @@
-import { useState } from "react";
-import SearchForm from "./SearchForm/SearchForm";
-import ArticleList from "./ArticleList/ArticleList";
-import { getArticles } from "../services/articleService";
-import { Article } from "../types/article";
+import OrderForm from "./OrderForm/OrderForm";
 
 export default function App() {
-  const [articles, setArticles] = useState<Article[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  const handleSearch = async (newTopic: string) => {
-    try {
-      setIsLoading(true);
-      setIsError(false);
-      const newArticles = await getArticles(newTopic);
-      setArticles(newArticles);
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
     <>
-      <SearchForm onSearch={handleSearch} />
-      {isLoading && <strong>Loading articles...</strong>}
-      {isError && <strong>Something went wrong please try again...</strong>}
-      {articles.length > 0 && <ArticleList items={articles} />}
+      <OrderForm />
     </>
   );
 }
