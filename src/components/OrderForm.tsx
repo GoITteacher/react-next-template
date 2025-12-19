3d55cd75ef32e4814e5cc4d69524189e957a998e
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-12-19 09:07:20 +0200

    update

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -1,192 +1,67 @@
-import { useId } from "react";
-import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
-// namespace import
-import * as Yup from "yup";
 import css from "./OrderForm.module.css";
 
-const OrderSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(3, "Too Short!")
-    .required("This field is required"),
-  email: Yup.string()
-    .email("Must be email format")
-    .required("This field is required"),
-  delivery: Yup.string()
-    .oneOf(["pickup", "courier", "drone"], "Invalid method")
-    .required("Delivery method is required"),
-  restrictions: Yup.array().of(
-    Yup.string().oneOf(["vegan", "gluten-free", "nut-free"])
-  ),
-  deliveryTime: Yup.string().required("Select time"),
-  message: Yup.string().max(250, "Too Long!"),
-});
-
-interface FormValues {
-  username: string;
-  email: string;
-  delivery: string;
-  restrictions: string[];
-  deliveryTime: string;
-  message: string;
-}
-
-const initialValues: FormValues = {
-  username: "",
-  email: "",
-  delivery: "pickup",
-  restrictions: [],
-  deliveryTime: "",
-  message: "",
-};
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export default function OrderForm() {
-  const fieldId = useId();
-
-  const handleSubmit = async (
-    values: FormValues,
-    actions: FormikHelpers<FormValues>
-  ) => {
-    await sleep(1000);
-    console.log("handleSubmit", values);
-    actions.resetForm();
-  };
-
-  // Render Prop
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={OrderSchema}
-      onSubmit={handleSubmit}
-    >
-      {({ isSubmitting }) => {
-        return (
-          <Form className={css.form}>
-            <fieldset className={css.fieldset}>
-              <legend className={css.legend}>Client Info</legend>
-              <label htmlFor={`${fieldId}-username`} className={css.label}>
-                Name
-              </label>
-              <Field
-                type="text"
-                name="username"
-                id={`${fieldId}-username`}
-                className={css.input}
-              />
-              <ErrorMessage
-                name="username"
-                component="span"
-                className={css.error}
-              />
-              <label htmlFor={`${fieldId}-email`} className={css.label}>
-                Email
-              </label>
-              <Field
-                type="email"
-                name="email"
-                id={`${fieldId}-email`}
-                className={css.input}
-              />
-              <ErrorMessage
-                name="email"
-                component="span"
-                className={css.error}
-              />
-            </fieldset>
+    <form className={css.form}>
+      <fieldset className={css.fieldset}>
+        <legend className={css.legend}>Client Info</legend>
+        <label className={css.label}>Name</label>
+        <input type="text" name="username" className={css.input} />
 
-            <fieldset className={css.fieldset}>
-              <legend className={css.legend}>Delivery method</legend>
+        <label className={css.label}>Email</label>
+        <input type="email" name="email" className={css.input} />
+      </fieldset>
 
-              <label className={css.option}>
-                <Field type="radio" name="delivery" value="pickup" />
-                Pickup
-              </label>
-              <label className={css.option}>
-                <Field type="radio" name="delivery" value="courier" />
-                Courier
-              </label>
-              <label className={css.option}>
-                <Field type="radio" name="delivery" value="drone" />
-                Drone delivery
-              </label>
-              <ErrorMessage
-                name="delivery"
-                component="span"
-                className={css.error}
-              />
-            </fieldset>
+      <fieldset className={css.fieldset}>
+        <legend className={css.legend}>Delivery method</legend>
 
-            <fieldset className={css.fieldset}>
-              <legend className={css.legend}>Dietary restrictions</legend>
+        <label className={css.option}>
+          <input type="radio" name="delivery" value="pickup" />
+          Pickup
+        </label>
+        <label className={css.option}>
+          <input type="radio" name="delivery" value="courier" />
+          Courier
+        </label>
+        <label className={css.option}>
+          <input type="radio" name="delivery" value="drone" />
+          Drone delivery
+        </label>
+      </fieldset>
 
-              <label className={css.option}>
-                <Field type="checkbox" name="restrictions" value="vegan" />
-                Vegan
-              </label>
-              <label className={css.option}>
-                <Field
-                  type="checkbox"
-                  name="restrictions"
-                  value="gluten-free"
-                />
-                Gluten-free
-              </label>
-              <label className={css.option}>
-                <Field type="checkbox" name="restrictions" value="nut-free" />
-                Nut-free
-              </label>
-              <ErrorMessage
-                name="restrictions"
-                component="span"
-                className={css.error}
-              />
-            </fieldset>
+      <fieldset className={css.fieldset}>
+        <legend className={css.legend}>Dietary restrictions</legend>
 
-            <label htmlFor={`${fieldId}-deliveryTime`} className={css.label}>
-              Preferred delivery time
-            </label>
-            <Field
-              as="select"
-              name="deliveryTime"
-              id={`${fieldId}-deliveryTime`}
-              className={css.input}
-            >
-              <option value="" disabled>
-                -- Choose delivery time --
-              </option>
-              <option value="morning">Morning (8:00-12:00)</option>
-              <option value="afternoon">Afternoon (12:00-16:00)</option>
-              <option value="evening">Evening (16:00-20:00)</option>
-            </Field>
-            <ErrorMessage
-              name="deliveryTime"
-              component="span"
-              className={css.error}
-            />
+        <label className={css.option}>
+          <input type="checkbox" name="restrictions" value="vegan" />
+          Vegan
+        </label>
+        <label className={css.option}>
+          <input type="checkbox" name="restrictions" value="gluten-free" />
+          Gluten-free
+        </label>
+        <label className={css.option}>
+          <input type="checkbox" name="restrictions" value="nut-free" />
+          Nut-free
+        </label>
+      </fieldset>
 
-            <label htmlFor={`${fieldId}-message`} className={css.label}>
-              Additional message
-            </label>
-            <Field
-              as="textarea"
-              name="message"
-              rows={4}
-              id={`${fieldId}-message`}
-              className={css.textarea}
-            />
-            <ErrorMessage
-              name="message"
-              component="span"
-              className={css.error}
-            />
+      <label className={css.label}>Preferred delivery time</label>
+      <select name="deliveryTime" className={css.input}>
+        <option value="" disabled>
+          -- Choose delivery time --
+        </option>
+        <option value="morning">Morning (8:00-12:00)</option>
+        <option value="afternoon">Afternoon (12:00-16:00)</option>
+        <option value="evening">Evening (16:00-20:00)</option>
+      </select>
 
-            <button type="submit" className={css.button}>
-              {isSubmitting ? "Submitting your order..." : "Place order"}
-            </button>
-          </Form>
-        );
-      }}
-    </Formik>
+      <label className={css.label}>Additional message</label>
+      <textarea name="message" rows={4} className={css.textarea}></textarea>
+
+      <button type="submit" className={css.button}>
+        Place order
+      </button>
+    </form>
   );
 }
