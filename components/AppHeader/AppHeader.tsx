667f1191b58a01e1310d6b32fa2f1d0302ac1835
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2026-01-13 08:21:47 +0200

    update

--- components/AppHeader/AppHeader.tsx
+++ components/AppHeader/AppHeader.tsx
@@ -2,12 +2,8 @@
 
 import Link from "next/link";
 import css from "./AppHeader.module.css";
-import { selectChangeLang, selectLang, useLangStore } from "@/stores/langStore";
 
 export default function AppHeader() {
-  const lang = useLangStore(selectLang);
-  const changeLang = useLangStore(selectChangeLang);
-
   return (
     <header className={css.header}>
       <ul className={css.nav}>
@@ -15,21 +11,15 @@ export default function AppHeader() {
           <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="/tasks">Tasks</Link>
+          <Link href="/products">Tasks</Link>
+        </li>
+        <li>
+          <Link href="/wishlist">Bucket</Link>
         </li>
         <li>
           <Link href="/bucket">Bucket</Link>
         </li>
       </ul>
-      <select
-        name="lang"
-        value={lang}
-        onChange={(e) => changeLang(e.target.value)}
-      >
-        <option value="en">En</option>
-        <option value="ua">Ua</option>
-        <option value="pl">Pl</option>
-      </select>
     </header>
   );
 }
